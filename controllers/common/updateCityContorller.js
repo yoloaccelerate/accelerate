e644f66fc18ceb908f842e0d102670469f63583e
Shourya Vikram Singh <vikramshourya03@gmail.com>
2020-12-15 00:05:43 +0530

    City duplication in dropdown removed

--- controllers/common/updateCityContorller.js
+++ controllers/common/updateCityContorller.js
@@ -7,31 +7,32 @@ const countryModel = require('../../model/countryModel');
 
 exports.updateCity = (req, res) => {
     countryModel.find({ name: req.body.name }, (err, city) => {
+        const arr = city[0].cities;
+        let registeredCity = req.body.city;
         if (err) {
             res.status(400).json(err);
-            console.log("error mein");
         }
         else {
-            // console.log(req.body.name);
-            countryModel.findOneAndUpdate({ name: req.body.name }, {
-                $push: {
-                    cities: req.body.city
+            let flag=0;
+            for(data of arr){
+                if(data.toLowerCase() == registeredCity.toLowerCase()){
+                    flag=1;
                 }
-            }, (error, updatedCity) => {
-                if (error) {
-                    res.status(400).json(error);
-                } else {
-                    ;
-                    res.status(200).json("City successfully updated.");
-                    // console.log("City added successfully");
-                }
-            })
+            }
+            if(flag==0){
+                countryModel.findOneAndUpdate({ name: req.body.name }, {
+                    $push: {
+                        cities: req.body.city
+                    }
+                }, (error, updatedCity) => {
+                    if (error) {
+                        res.status(400).json(error);
+                    } else {
+                        res.status(200).json("City successfully updated.");
+                    }
+                })
+            }
+            
         }
-        // const updatedCities = countryModel.find({ name: req.body.name });
-        // if(updatedCities.length>0){
-        //     console.log(updatedCities);
-        // }else{
-        //     console.log("Nothing Found");
-        // }
     })
 }
